64d922b4230e326716b33f6cc2dc8ead608a6806
xz <95953670+laagginq@users.noreply.github.com>
2023-08-04 17:42:24 -0700

    Update skids.lua

--- skids.lua
+++ skids.lua
@@ -1,19 +1,3 @@
-local ScreenGui = Instance.new("ScreenGui")
-local TextLabel = Instance.new("TextLabel")
-ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-TextLabel.Parent = ScreenGui
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(-0.193263829, 0, 0.420792073, 0)
-TextLabel.Size = UDim2.new(1.38572574, 0, 0.534653485, 0)
-TextLabel.Font = Enum.Font.SourceSansBold
-TextLabel.Text = "get yo money up not yo funny up"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 14.000
-TextLabel.TextWrapped = true
-wait(1)
+game.ReplicatedStorage.MainEvent:FireServer("OneMoreTime")
+wait(2)
 game.Players.LocalPlayer:kick("FUCKING BROKIE YOU HAVE BEEN BANNED BUY EVO V3 @ .gg/camlock - xz <3 ")
